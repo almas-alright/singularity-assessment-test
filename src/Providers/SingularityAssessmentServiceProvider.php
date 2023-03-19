f70c8040d227fcc88381ab9872f3a28e1dca8dc0
almas-alright <almas-alright@users.noreply.github.com>
2023-03-19 20:00:52 +0000

    Fix styling

--- src/Providers/SingularityAssessmentServiceProvider.php
+++ src/Providers/SingularityAssessmentServiceProvider.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace AlmasAlright\SingularityAssessmentTest\Providers;
+
 use Illuminate\Support\ServiceProvider;
 
 class SingularityAssessmentServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
     }
 
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'team-members');
     }
 }
