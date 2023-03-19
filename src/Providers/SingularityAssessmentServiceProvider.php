77c2f3a265e62e1094ee527689029e640629a11e
almas-alright <almas-alright@users.noreply.github.com>
2023-03-19 18:56:08 +0000

    Fix styling

--- src/Providers/SingularityAssessmentServiceProvider.php
+++ src/Providers/SingularityAssessmentServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace AlmasAlright\SingularityAssessmentTest\Providers;
+
 use Closure;
 use Illuminate\Support\ServiceProvider;
 
@@ -8,7 +9,6 @@ class SingularityAssessmentServiceProvider extends ServiceProvider
 {
     public function register()
     {
-
     }
 
     public function boot(Closure $callback)
@@ -17,11 +17,10 @@ class SingularityAssessmentServiceProvider extends ServiceProvider
             // Export the migration
             if (! class_exists('CreatePostsTable')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_team_members_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_team_members_table.php'),
+                    __DIR__.'/../database/migrations/create_team_members_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_team_members_table.php'),
                     // you can add any number of migrations here
                 ], 'migrations');
             }
         }
-
     }
 }
