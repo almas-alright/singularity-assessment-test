bbc853b9369256bb864d6a25c99ed50f32b40de1
almas_alright <kutsnalmas@gmail.com>
2023-03-20 09:56:46 +0600

    package covered

--- src/Providers/SingularityAssessmentServiceProvider.php
+++ src/Providers/SingularityAssessmentServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace AlmasAlright\SingularityAssessmentTest\Providers;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 
 class SingularityAssessmentServiceProvider extends ServiceProvider
 {
@@ -14,5 +15,6 @@ class SingularityAssessmentServiceProvider extends ServiceProvider
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'team-members');
+        Artisan::call('migrate', ['--path' => __DIR__ . '/../database/migrations']);
     }
 }
